9fd3afe9c313e52ab6a4a4954287d4d425ab32a4
azadtom <kumarazad2917@gmail.com>
2023-12-25 23:00:58 +0530

    changes in theme

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -8,7 +8,7 @@ const BlogCard=({index,img})=>{
         <>
         <div className="flex justify-center items-center relative rounded-2xl"> 
         <img src={img} alt="blog"  className=" w-full h-[300px]   bg-center bg-cover object-cover  rounded-2xl "/>
-            <h2 className="absolute text-8xl font-semibold z-10 text-[var(--primarycolor)]">{index+1}</h2>
+            <h2 className="absolute text-8xl font-semibold z-10 text-white">{index+1}</h2>
         </div>
         </>
     )
