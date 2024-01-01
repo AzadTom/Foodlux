13cdf34b1f7b764887f4eed03cec3298df5cb6ce
azadtom <kumarazad2917@gmail.com>
2024-01-01 15:58:01 +0530

    searchiing implement using redux-toolkit

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productReducer from '../reducers/productSlice.js';
 import cartReducer from '../reducers/cartSlice.js';
+import filterReducer from '../reducers/filterSlice.js';
 
 const store = configureStore({
 
     reducer:{
         product:productReducer,
-        cart:cartReducer
+        cart:cartReducer,
+        filterData:filterReducer
     }
 })
 
